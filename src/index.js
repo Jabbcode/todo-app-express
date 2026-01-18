e9d6cc8dd625da31cecf210593b15b9af990ea30
Jabbcode <jabbsontouch@gmail.com>
2026-01-18 15:47:45 +0100

    feat: add logget basic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import { requestLogger } from './middleware/logger.middleware.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/api/todos', todosRouter);
 
